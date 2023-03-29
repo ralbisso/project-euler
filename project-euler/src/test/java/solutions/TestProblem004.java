2fa8b6764a86642728c545228833e5bac02e6cb0
romain albisson <albisson.romain@gmail.com>
2023-03-29 18:42:41 +0200

    Problem 4 - Largest palindrome product

---
+++ project-euler/src/test/java/solutions/TestProblem004.java
@@ -0,0 +1,12 @@
+package solutions;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestProblem004 {
+
+    @Test
+    public void test() {
+        Assert.assertEquals(906609, new Problem004().solve());
+    }
+}
