4fb58c4a4da0bcb4c89bbfe7801b497e1b5d58f2
romain albisson <albisson.romain@gmail.com>
2023-03-29 18:36:23 +0200

    Problem 3 - Largest prime factor

---
+++ project-euler/src/main/java/solutions/Problem003.java
@@ -0,0 +1,16 @@
+package solutions;
+
+public class Problem003 {
+
+    public int solve() {
+        long number = 600851475143L;
+        int factor = 1;
+        while (number > 1) {
+            factor += 2;
+            while (number % factor == 0) {
+                number /= factor;
+            }
+        }
+        return factor;
+    }
+}
---
+++ project-euler/src/test/java/solutions/TestProblem003.java
@@ -0,0 +1,12 @@
+package solutions;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestProblem003 {
+
+    @Test
+    public void test() {
+        Assert.assertEquals(6857, new Problem003().solve());
+    }
+}
