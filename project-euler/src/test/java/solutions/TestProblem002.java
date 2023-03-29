06f069c50134383e20dc96b945b5f5b744f0cf6e
romain albisson <albisson.romain@gmail.com>
2023-03-29 18:32:15 +0200

    Problem 2 - Even Fibonacci numbers

---
+++ project-euler/src/test/java/solutions/TestProblem002.java
@@ -0,0 +1,12 @@
+package solutions;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestProblem002 {
+
+    @Test
+    public void test() {
+        Assert.assertEquals(4613732, new Problem002().solve());
+    }
+}
