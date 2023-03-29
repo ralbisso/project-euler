4898fd604f3b56c6453c4aad338b5bfa75ecd907
romain albisson <albisson.romain@gmail.com>
2023-03-29 17:46:21 +0200

    Problem 1 - Multiples of 3 or 5

---
+++ project-euler/src/main/java/solutions/Problem001.java
@@ -0,0 +1,8 @@
+package solutions;
+
+public class Problem001 {
+
+    public int solve() {
+        return 0;
+    }
+}
---
+++ project-euler/src/test/java/solutions/TestProblem001.java
@@ -0,0 +1,12 @@
+package solutions;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestProblem001 {
+
+    @Test
+    public void test() {
+        Assert.assertEquals(10, new Problem001().solve());
+    }
+}
