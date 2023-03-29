2fef996ecb8129720b58c48f6a06134a8d701f5a
romain albisson <albisson.romain@gmail.com>
2023-03-29 19:00:35 +0200

    Problem 5 - Smallest multiple

---
+++ project-euler/src/test/java/solutions/TestProblem005.java
@@ -0,0 +1,12 @@
+package solutions;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestProblem005 {
+
+    @Test
+    public void test() {
+        Assert.assertEquals(232792560, new Problem005().solve());
+    }
+}
