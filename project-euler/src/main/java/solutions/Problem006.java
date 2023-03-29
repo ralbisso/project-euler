350a888553b2dbdf21346b6119793a747c24c74a
romain albisson <albisson.romain@gmail.com>
2023-03-29 19:10:48 +0200

    Problem 6 - Sum square difference

---
+++ project-euler/src/main/java/solutions/Problem006.java
@@ -0,0 +1,11 @@
+package solutions;
+
+public class Problem006 {
+
+    public int solve() {
+        int sumOfFirst100Numbers = 100 * (100 + 1) / 2;
+        int sumOfFirst100Squares = 100 * (100 + 1) * (2 * 100 + 1) / 6;
+        int sumOfFirst100NumbersSquared = sumOfFirst100Numbers * sumOfFirst100Numbers;
+        return sumOfFirst100NumbersSquared - sumOfFirst100Squares;
+    }
+}
---
+++ project-euler/src/test/java/solutions/TestProblem006.java
@@ -0,0 +1,12 @@
+package solutions;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestProblem006 {
+
+    @Test
+    public void test() {
+        Assert.assertEquals(25164150, new Problem006().solve());
+    }
+}
