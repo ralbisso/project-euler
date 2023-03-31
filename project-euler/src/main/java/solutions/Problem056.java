dde022b59a8bed0a362e6b98d170dad47c6e81a5
romain albisson <albisson.romain@gmail.com>
2023-03-31 21:18:20 +0200

    Problem 56: Powerful digit sum

--- project-euler/src/main/java/solutions/Problem056.java
+++ project-euler/src/main/java/solutions/Problem056.java
@@ -1,8 +1,22 @@
 package solutions;
 
+import static utils.NumberUtils.getSumOfDigits;
+
+import java.math.BigInteger;
+
 public class Problem056 {
 
     public int solve() {
-        return 0;
+        int max = 0;
+        for (int a = 2; a < 100; a++) {
+            for (int b = 2; b < 100; b++) {
+                BigInteger big = BigInteger.valueOf(a).pow(b);
+                int sum = getSumOfDigits(big.toString());
+                if (sum > max) {
+                    max = sum;
+                }
+            }
+        }
+        return max;
     }
 }
