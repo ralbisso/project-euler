f73159c10aed7b52170b087104cbe5acf616ad08
romain albisson <albisson.romain@gmail.com>
2023-03-31 10:22:20 +0200

    Problem 97: Large non-Mersenne prime

--- project-euler/src/main/java/solutions/Problem097.java
+++ project-euler/src/main/java/solutions/Problem097.java
@@ -2,7 +2,11 @@ package solutions;
 
 public class Problem097 {
 
-    public int solve() {
-        return 0;
+    public long solve() {
+        long prime = 28433;
+        for (int i = 0; i < 7830457; i++) {
+            prime = (prime * 2) % 10000000000L;
+        }
+        return prime + 1;
     }
 }
