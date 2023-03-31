e23b6baa7d5bd5d0791e43b0910461082820b05a
romain albisson <albisson.romain@gmail.com>
2023-03-31 10:59:50 +0200

    Problem 19: Counting Sundays

--- project-euler/src/main/java/solutions/Problem019.java
+++ project-euler/src/main/java/solutions/Problem019.java
@@ -1,8 +1,21 @@
 package solutions;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+
 public class Problem019 {
 
     public int solve() {
-        return 0;
+        int sundays = 0;
+        LocalDate date = LocalDate.of(1901, Month.JANUARY, 1);
+        LocalDate end = LocalDate.of(2000, Month.DECEMBER, 31);
+        while (date.isBefore(end)) {
+            date = date.plusMonths(1);
+            if (DayOfWeek.SUNDAY.equals(date.getDayOfWeek())) {
+                sundays++;
+            }
+        }
+        return sundays;
     }
 }
