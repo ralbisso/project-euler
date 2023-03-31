13df23a9cbc8ce23010ea476437de48fb2c01cf2
romain albisson <albisson.romain@gmail.com>
2023-03-31 14:39:43 +0200

    import static

--- project-euler/src/main/java/solutions/Problem010.java
+++ project-euler/src/main/java/solutions/Problem010.java
@@ -1,12 +1,12 @@
 package solutions;
 
-import utils.PrimeUtils;
+import static utils.PrimeUtils.getSieveOfEratosthenes;
 
 public class Problem010 {
 
     public long solve() {
         long sum = 0;
-        boolean[] primes = PrimeUtils.getSieveOfEratosthenes(2000000);
+        boolean[] primes = getSieveOfEratosthenes(2000000);
         for (int i = 0; i < primes.length; i++) {
             if (primes[i]) {
                 sum += i;
