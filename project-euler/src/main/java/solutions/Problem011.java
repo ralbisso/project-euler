0eab2fcd1cbb795ed0e61e8a66cafb48b7fe3aae
romain albisson <albisson.romain@gmail.com>
2023-03-31 16:12:43 +0200

    Problem 11: Largest product in a grid

--- project-euler/src/main/java/solutions/Problem011.java
+++ project-euler/src/main/java/solutions/Problem011.java
@@ -1,8 +1,50 @@
 package solutions;
 
+import static utils.FileUtils.P011;
+import static utils.FileUtils.getAllLinesAsGrid;
+
 public class Problem011 {
 
     public int solve() {
+        int[][] grid = getAllLinesAsGrid(P011);
+        int length = grid.length, max = 0;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                if (grid[i][j] > 0) {
+                    int maxProduct = getPointMaxProduct(i, j, grid);
+                    if (maxProduct > max) {
+                        max = maxProduct;
+                    }
+                }
+            }
+        }
+        return max;
+    }
+
+    private int getPointMaxProduct(int i, int j, int[][] grid) {
+        int antidiagonal = 1, diagonal = 1, horizontal = 1, vertical = 1;
+        for (int k = 0; k < 4; k++) {
+            antidiagonal *= getNextTerm(grid, i + k, j - k);
+            diagonal *= getNextTerm(grid, i + k, j + k);
+            horizontal *= getNextTerm(grid, i, j + k);
+            vertical *= getNextTerm(grid, i + k, j);
+        }
+        return getMax(antidiagonal, diagonal, horizontal, vertical);
+    }
+
+    private int getNextTerm(int[][] grid, int i, int j) {
+        if (i < grid.length && j >= 0 && j < grid.length) {
+            return grid[i][j];
+        }
         return 0;
+    }
+
+    private int getMax(int antidiagonal, int diagonal, int horizontal, int vertical) {
+        int max = 0;
+        max = Math.max(max, antidiagonal);
+        max = Math.max(max, diagonal);
+        max = Math.max(max, horizontal);
+        max = Math.max(max, vertical);
+        return max;
     }
 }
--- project-euler/src/main/java/utils/FileUtils.java
+++ project-euler/src/main/java/utils/FileUtils.java
@@ -11,9 +11,13 @@ public class FileUtils {
     private final static String RESOURCES = "src/main/resources/";
 
     public final static String P008 = "p008_series";
+    public final static String P011 = "p011_grid";
     public final static String P013 = "p013_large_sum";
     public final static String P018 = "p018_triangle";
     public final static String P067 = "p067_triangle";
+    public final static String P081 = "p081_matrix";
+    public final static String P082 = "p082_matrix";
+    public final static String P083 = "p083_matrix";
     public final static String P099 = "p099_base_exp";
 
     public static List<String> getAllLines(String fileName) {
@@ -25,5 +29,19 @@ public class FileUtils {
             System.out.format("I/O error: %s%n", e);
         }
         return lines;
+    }
+
+    public static int[][] getAllLinesAsGrid(String fileName) {
+        var input = getAllLines(fileName);
+        var grid = new int[input.size()][];
+        for (int i = 0; i < grid.length; i++) {
+            var split = input.get(i).split(" ");
+            var row = new int[split.length];
+            for (int j = 0; j < row.length; j++) {
+                row[j] = Integer.parseInt(split[j]);
+            }
+            grid[i] = row;
+        }
+        return grid;
     }
 }
