afec5b4e3304b533cf9cec73b89c30dd087f7746
romain albisson <albisson.romain@gmail.com>
2023-03-31 12:23:31 +0200

    Problem 8: Largest product in a series

--- project-euler/src/main/java/solutions/Problem008.java
+++ project-euler/src/main/java/solutions/Problem008.java
@@ -1,8 +1,31 @@
 package solutions;
 
+import utils.FileUtils;
+
 public class Problem008 {
 
-    public int solve() {
-        return 0;
+    public long solve() {
+        long max = 0;
+        String series = FileUtils.getAllLines(FileUtils.P008)
+                .stream()
+                .reduce("", (s1, s2) -> s1 + s2);
+        for (int i = 0; i <= series.length() - 13; i++) {
+            long product = getProduct(series.substring(i, i + 13));
+            if (product > max) {
+                max = product;
+            }
+        }
+        return max;
+    }
+
+    private long getProduct(String substring) {
+        if (substring.contains("0")) {
+            return 0;
+        }
+        long product = 1;
+        for (char c : substring.toCharArray()) {
+            product *= Character.getNumericValue(c);
+        }
+        return product;
     }
 }
