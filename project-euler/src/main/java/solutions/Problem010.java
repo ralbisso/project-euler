6b6fae12559826bb2d009818d43759ff0882bf6b
romain albisson <albisson.romain@gmail.com>
2023-03-31 12:05:17 +0200

    Problem 10: Summation of primes

--- project-euler/src/main/java/solutions/Problem010.java
+++ project-euler/src/main/java/solutions/Problem010.java
@@ -1,8 +1,32 @@
 package solutions;
 
+import java.util.Arrays;
+
 public class Problem010 {
 
-    public int solve() {
-        return 0;
+    public long solve() {
+        long sum = 0;
+        boolean[] primes = sieveOfEratosthenes(2000000);
+        for (int i = 0; i < primes.length; i++) {
+            if (primes[i]) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    private boolean[] sieveOfEratosthenes(int n) {
+        boolean[] primes = new boolean[n + 1];
+        Arrays.fill(primes, true);
+        primes[0] = false;
+        primes[1] = false;
+        for (int i = 2; i * i <= n; i++) {
+            if (primes[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    primes[j] = false;
+                }
+            }
+        }
+        return primes;
     }
 }
