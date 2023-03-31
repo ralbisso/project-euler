1620fe50f4ef8888c48b8740d5f5178bd46ae413
romain albisson <albisson.romain@gmail.com>
2023-03-31 20:45:33 +0200

    Problem 14: Longest Collatz sequence

--- project-euler/src/main/java/solutions/Problem014.java
+++ project-euler/src/main/java/solutions/Problem014.java
@@ -3,6 +3,23 @@ package solutions;
 public class Problem014 {
 
     public int solve() {
-        return 0;
+        int max = 1, maxLength = 1;
+        for (int i = 1; i < 1000000; i++) {
+            long number = i;
+            int length = 1;
+            while (number > 1) {
+                number = getNextCollatzTerm(number);
+                length++;
+            }
+            if (length > maxLength) {
+                maxLength = length;
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    private long getNextCollatzTerm(long number) {
+        return number % 2 == 0 ? number / 2 : 3 * number + 1;
     }
 }
