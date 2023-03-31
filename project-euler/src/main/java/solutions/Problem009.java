79a41d2a42e8fca1b92ad416abb68fdca13a30c6
romain albisson <albisson.romain@gmail.com>
2023-03-31 11:27:19 +0200

    Problem 9 - minor change

--- project-euler/src/main/java/solutions/Problem009.java
+++ project-euler/src/main/java/solutions/Problem009.java
@@ -7,7 +7,7 @@ public class Problem009 {
     public int solve() {
         int perimeter = 1000;
         for (int a = 3; a < perimeter; a++) {
-            for (int b = a + 1; b < perimeter; b++) {
+            for (int b = a + 1; a + b < perimeter; b++) {
                 int c = perimeter - (a + b);
                 var triplet = new Triplet(a, b, c);
                 if (triplet.isPythagorean()) {
