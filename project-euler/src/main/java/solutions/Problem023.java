caaa96d748f8405bcba1e3195fb3358fd97d68a1
romain albisson <albisson.romain@gmail.com>
2023-04-03 12:26:33 +0200

    Problem 23: Non-abundant sums

--- project-euler/src/main/java/solutions/Problem023.java
+++ project-euler/src/main/java/solutions/Problem023.java
@@ -1,8 +1,45 @@
 package solutions;
 
+import static utils.DivisorsUtils.getSumOfDivisors;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem023 {
 
     public int solve() {
-        return 0;
+        int sum = 0;
+        boolean[] canBeWrittenAsSumTwoAbundantNumbers = getNumbersThatCanBeWrittenAsSumOfTwoAbundantNumbers(
+                28123);
+        for (int i = 1; i < canBeWrittenAsSumTwoAbundantNumbers.length; i++) {
+            if (!canBeWrittenAsSumTwoAbundantNumbers[i]) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    private boolean[] getNumbersThatCanBeWrittenAsSumOfTwoAbundantNumbers(int limit) {
+        boolean[] canBeWrittenAsSumTwoAbundantNumbers = new boolean[limit + 1];
+        List<Integer> abundantNumbers = getAbundantNumbers(28123);
+        for (int a : abundantNumbers) {
+            for (int b : abundantNumbers) {
+                if (a + b < canBeWrittenAsSumTwoAbundantNumbers.length) {
+                    canBeWrittenAsSumTwoAbundantNumbers[a + b] = true;
+                }
+            }
+        }
+        return canBeWrittenAsSumTwoAbundantNumbers;
+    }
+
+    private List<Integer> getAbundantNumbers(int limit) {
+        List<Integer> abundantNumbers = new ArrayList<>();
+        int[] sumOfDivisors = getSumOfDivisors(limit);
+        for (int i = 2; i < sumOfDivisors.length; i++) {
+            if (i < sumOfDivisors[i]) {
+                abundantNumbers.add(i);
+            }
+        }
+        return abundantNumbers;
     }
 }
