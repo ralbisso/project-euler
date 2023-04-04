d68771b34909aae22966fe8606fd8f8d1babbefa
romain albisson <albisson.romain@gmail.com>
2023-04-04 19:36:45 +0200

    Add limit variable

--- project-euler/src/main/java/solutions/Problem014.java
+++ project-euler/src/main/java/solutions/Problem014.java
@@ -3,13 +3,13 @@ package solutions;
 public class Problem014 {
 
     public int solve() {
-        int max = 1, longest = 1;
-        int[] memo = new int[1000000];
-        for (int start = 1; start < 1000000; start++) {
+        int max = 1, longest = 1, limit = 1000000;
+        int[] memo = new int[limit];
+        for (int start = 1; start < limit; start++) {
             long collatz = start;
             int length = 0;
             while (collatz > 1) {
-                if (collatz < 1000000 && memo[(int) collatz] != 0) {
+                if (collatz < length && memo[(int) collatz] != 0) {
                     length += memo[(int) collatz];
                     break;
                 }
