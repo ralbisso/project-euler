b422d21828c073e3aa39580ceed67f1da7a87d3f
romain albisson <albisson.romain@gmail.com>
2023-04-04 18:08:26 +0200

    Optimization - use memoization

--- project-euler/src/main/java/solutions/Problem014.java
+++ project-euler/src/main/java/solutions/Problem014.java
@@ -4,13 +4,19 @@ public class Problem014 {
 
     public int solve() {
         int max = 1, longest = 1;
+        int[] memo = new int[1000000];
         for (int start = 1; start < 1000000; start++) {
             long collatz = start;
-            int length = 1;
+            int length = 0;
             while (collatz > 1) {
+                if (collatz < 1000000 && memo[(int) collatz] != 0) {
+                    length += memo[(int) collatz];
+                    break;
+                }
                 collatz = getNextCollatzTerm(collatz);
                 length++;
             }
+            memo[start] = length;
             if (length > longest) {
                 longest = length;
                 max = start;
