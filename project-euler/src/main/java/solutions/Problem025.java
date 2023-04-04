b07f3c3aa63f9652e5f73da1e9e4685745f904a8
romain albisson <albisson.romain@gmail.com>
2023-04-04 20:00:34 +0200

    Problem 25: 1000-digit Fibonacci number

--- project-euler/src/main/java/solutions/Problem025.java
+++ project-euler/src/main/java/solutions/Problem025.java
@@ -1,8 +1,18 @@
 package solutions;
 
+import java.math.BigInteger;
+
 public class Problem025 {
 
     public int solve() {
-        return 0;
+        BigInteger[] fibonacci = { BigInteger.ZERO, BigInteger.ONE };
+        int index = 1;
+        while (fibonacci[1].toString().length() < 1000) {
+            BigInteger next = fibonacci[0].add(fibonacci[1]);
+            fibonacci[0] = fibonacci[1];
+            fibonacci[1] = next;
+            index++;
+        }
+        return index;
     }
 }
