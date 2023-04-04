c2dcb4a24b497e8acb1b51e756721bab50d683bc
romain albisson <albisson.romain@gmail.com>
2023-04-04 21:47:03 +0200

    Problem 100: Arranged probability

--- project-euler/src/main/java/solutions/Problem100.java
+++ project-euler/src/main/java/solutions/Problem100.java
@@ -2,7 +2,25 @@ package solutions;
 
 public class Problem100 {
 
-    public int solve() {
-        return 0;
+    public long solve() {
+        /*
+         * P(BB) = (B/T)*((B-1)/(T-1)) = 0.5
+         * 2B(B-1) = T(T-1)
+         * 2B²-T²-2B + T = 0 or 2x² - y² - 2x + y = 0
+         * 
+         * Using a quadratic diophantine equation solver
+         * Initial solution: (x0 = 1, y0 = 1)
+         *  
+         * Recursive solutions:
+         * (xN+1, yN+1) = (3xN + 2yN - 2, 4xN + 3yN - 3)
+         */
+        long blue = 1, total = 1;
+        while (total < 1000000000000L) {
+            long blueTmp = blue;
+            long totalTmp = total;
+            blue = 3 * blueTmp + 2 * totalTmp - 2;
+            total = 4 * blueTmp + 3 * totalTmp - 3;
+        }
+        return blue;
     }
 }
