be2c9be2f7b9c7658b27c3c25a9166bf94ad6844
romain albisson <albisson.romain@gmail.com>
2023-04-05 12:25:44 +0200

    Problem 31: Coin sums

--- project-euler/src/main/java/solutions/Problem031.java
+++ project-euler/src/main/java/solutions/Problem031.java
@@ -3,6 +3,15 @@ package solutions;
 public class Problem031 {
 
     public int solve() {
-        return 0;
+        int value = 200;
+        int[] coins = { 1, 2, 5, 10, 20, 50, 100, 200 };
+        var ways = new int[value + 1];
+        ways[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= value; i++) {
+                ways[i] += ways[i - coin];
+            }
+        }
+        return ways[value];
     }
 }
