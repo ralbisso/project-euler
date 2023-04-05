9911222e66bcb00ab88385578653392dc9c031ea
romain albisson <albisson.romain@gmail.com>
2023-04-05 11:59:03 +0200

    Problem 28: Number spiral diagonals

--- project-euler/src/main/java/solutions/Problem028.java
+++ project-euler/src/main/java/solutions/Problem028.java
@@ -3,6 +3,13 @@ package solutions;
 public class Problem028 {
 
     public int solve() {
-        return 0;
+        int count = 1, sum = 1, increment = 2, spiral = 1001;
+        for (int number = 3; number <= spiral * spiral; number += increment) {
+            if (count++ % 4 == 0) {
+                increment += 2;
+            }
+            sum += number;
+        }
+        return sum;
     }
 }
