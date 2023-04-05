f3a6eb28a332d13d738c84b77a8055c1ed003613
romain albisson <albisson.romain@gmail.com>
2023-04-05 12:19:55 +0200

    Problem 30: Digit fifth powers

--- project-euler/src/main/java/solutions/Problem030.java
+++ project-euler/src/main/java/solutions/Problem030.java
@@ -3,6 +3,27 @@ package solutions;
 public class Problem030 {
 
     public int solve() {
-        return 0;
+        int sum = 0;
+        int[] fifthPowers = getDigitFifthPowers();
+        for (int i = 2; i <= 6 * fifthPowers[9]; i++) {
+            int number = i, sumOfDigitFifthPowers = 0;
+            while (number > 0) {
+                int digit = number % 10;
+                number /= 10;
+                sumOfDigitFifthPowers += fifthPowers[digit];
+            }
+            if (sumOfDigitFifthPowers == i) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    private int[] getDigitFifthPowers() {
+        int[] fifthPowers = new int[10];
+        for (int i = 0; i < fifthPowers.length; i++) {
+            fifthPowers[i] = (int) Math.pow(i, 5);
+        }
+        return fifthPowers;
     }
 }
