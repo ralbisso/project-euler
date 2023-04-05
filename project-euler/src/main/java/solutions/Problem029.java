42d6858bc71fc41d397d6c902d4d41b929efe5aa
romain albisson <albisson.romain@gmail.com>
2023-04-05 12:05:00 +0200

    Add var

--- project-euler/src/main/java/solutions/Problem029.java
+++ project-euler/src/main/java/solutions/Problem029.java
@@ -2,13 +2,12 @@ package solutions;
 
 import java.math.BigInteger;
 import java.util.HashSet;
-import java.util.Set;
 
 public class Problem029 {
 
     public int solve() {
         int limit = 100;
-        Set<BigInteger> distinctPowers = new HashSet<>();
+        var distinctPowers = new HashSet<>();
         for (int a = 2; a <= limit; a++) {
             for (int b = 2; b <= limit; b++) {
                 BigInteger power = BigInteger.valueOf(a).pow(b);
