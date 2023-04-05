93509832bc2bb1af1940e87b759ee99def868738
romain albisson <albisson.romain@gmail.com>
2023-04-05 10:06:24 +0200

    Problem 26: Reciprocal cycles

--- project-euler/src/main/java/solutions/Problem026.java
+++ project-euler/src/main/java/solutions/Problem026.java
@@ -1,8 +1,26 @@
 package solutions;
 
+import static utils.PrimeUtils.getSieveOfEratosthenes;
+
 public class Problem026 {
 
     public int solve() {
-        return 0;
+        // We know that primes will generate the longest cycles
+        int number = 1, max = 0;
+        boolean[] primes = getSieveOfEratosthenes(1000);
+        for (int i = 2; i < primes.length; i++) {
+            if (primes[i]) {
+                int cycle = 1, remainder = 10 % i;
+                while (remainder != 1 && cycle < i - 1) {
+                    remainder = (remainder * 10) % i;
+                    cycle++;
+                }
+                if (cycle > max) {
+                    max = cycle;
+                    number = i;
+                }
+            }
+        }
+        return number;
     }
 }
