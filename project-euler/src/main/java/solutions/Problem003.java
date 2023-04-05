5aab321796f9ce138effdafb4b2a6bf802c7d3de
romain albisson <albisson.romain@gmail.com>
2023-04-05 09:21:24 +0200

    Add comment

--- project-euler/src/main/java/solutions/Problem003.java
+++ project-euler/src/main/java/solutions/Problem003.java
@@ -3,6 +3,7 @@ package solutions;
 public class Problem003 {
 
     public int solve() {
+        // The number is odd, no need to check if 2 is a factor
         long number = 600851475143L;
         int factor = 1;
         while (number > 1) {
