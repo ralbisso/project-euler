35eb82aaa1a228ea78f5be329d5895abf7c5f7ad
romain albisson <albisson.romain@gmail.com>
2023-04-05 12:04:28 +0200

    Problem 29: Distinct powers

--- project-euler/src/main/java/solutions/Problem029.java
+++ project-euler/src/main/java/solutions/Problem029.java
@@ -1,8 +1,20 @@
 package solutions;
 
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Problem029 {
 
     public int solve() {
-        return 0;
+        int limit = 100;
+        Set<BigInteger> distinctPowers = new HashSet<>();
+        for (int a = 2; a <= limit; a++) {
+            for (int b = 2; b <= limit; b++) {
+                BigInteger power = BigInteger.valueOf(a).pow(b);
+                distinctPowers.add(power);
+            }
+        }
+        return distinctPowers.size();
     }
 }
