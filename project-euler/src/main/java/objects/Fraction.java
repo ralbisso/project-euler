d643618c17ec393b9b5d8bcfe8f5eba9e28274d7
romain albisson <albisson.romain@gmail.com>
2023-04-06 14:01:26 +0200

    Change fraction

--- project-euler/src/main/java/objects/Fraction.java
+++ project-euler/src/main/java/objects/Fraction.java
@@ -6,15 +6,10 @@ public final class Fraction {
 
     private final int numerator;
     private final int denominator;
-    private final int numeratorInLCT;
-    private final int denominatorInLCT;
 
     public Fraction(int numerator, int denominator) {
         this.numerator = numerator;
         this.denominator = denominator;
-        int gcd = (int) getGCD(numerator, denominator);
-        this.numeratorInLCT = numerator / gcd;
-        this.denominatorInLCT = denominator / gcd;
     }
 
     public int getNumerator() {
@@ -25,19 +20,27 @@ public final class Fraction {
         return denominator;
     }
 
-    public int getDenominatorInLCT() {
-        return denominatorInLCT;
+    public Fraction multiply(Fraction fraction) {
+        int numerator = this.numerator * fraction.numerator;
+        int denominator = this.denominator * fraction.denominator;
+        return new Fraction(numerator, denominator);
+    }
+
+    public Fraction simplify() {
+        int gcd = (int) getGCD(numerator, denominator);
+        return new Fraction(numerator / gcd, denominator / gcd);
     }
 
     @Override
-    public boolean equals(Object other) {
-        if (this == other) {
+    public boolean equals(Object that) {
+        if (this == that) {
             return true;
         }
-        if (other instanceof Fraction) {
-            Fraction fraction = (Fraction) other;
-            return this.numeratorInLCT == fraction.numeratorInLCT
-                    && this.denominatorInLCT == fraction.denominatorInLCT;
+        if (that instanceof Fraction) {
+            Fraction thisSimplified = simplify();
+            Fraction thatSimplified = ((Fraction) that).simplify();
+            return thisSimplified.numerator == thatSimplified.numerator
+                    && thisSimplified.denominator == thatSimplified.denominator;
         }
         return false;
     }
