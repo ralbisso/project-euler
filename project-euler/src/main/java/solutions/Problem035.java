471c2a5c39f5ef545be71d22c10a7292847c6aac
romain albisson <albisson.romain@gmail.com>
2023-04-06 14:38:19 +0200

    Problem 35: Circular primes

--- project-euler/src/main/java/solutions/Problem035.java
+++ project-euler/src/main/java/solutions/Problem035.java
@@ -1,8 +1,40 @@
 package solutions;
 
+import static utils.PrimeUtils.getSieveOfEratosthenes;
+
 public class Problem035 {
 
     public int solve() {
-        return 0;
+        int count = 0;
+        boolean[] primes = getSieveOfEratosthenes(1000000);
+        for (int i = 2; i < primes.length; i++) {
+            if (primes[i]) {
+                int[] rotations = getRotations(i);
+                boolean isCircularPrime = true;
+                for (int j = 1; j < rotations.length; j++) {
+                    if (!primes[rotations[j]]) {
+                        isCircularPrime = false;
+                        break;
+                    }
+                }
+                if (isCircularPrime) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private int[] getRotations(int number) {
+        int size = (int) Math.log10(number) + 1;
+        int offset = (int) Math.pow(10, size - 1);
+        int[] rotations = new int[size];
+        rotations[0] = number;
+        for (int i = 1; i < rotations.length; i++) {
+            int last = number % 10;
+            number = last * offset + number / 10;
+            rotations[i] = number;
+        }
+        return rotations;
     }
 }
