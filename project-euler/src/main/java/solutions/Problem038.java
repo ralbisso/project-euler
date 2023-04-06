430a18ebe52c0888d2741599e9f2e660272d6ef6
romain albisson <albisson.romain@gmail.com>
2023-04-06 15:39:20 +0200

    Problem 38: Pandigital multiples

--- project-euler/src/main/java/solutions/Problem038.java
+++ project-euler/src/main/java/solutions/Problem038.java
@@ -1,8 +1,23 @@
 package solutions;
 
+import static utils.NumberUtils.is1To9Pandigital;
+
 public class Problem038 {
 
     public int solve() {
-        return 0;
+        int max = 0;
+        for (int i = 1; i < 10000; i++) {
+            int concatenated = i, multiples = i;
+            while (concatenated < 100000000) {
+                multiples += i;
+                int size = (int) Math.log10(multiples);
+                concatenated *= (int) Math.pow(10, size + 1);
+                concatenated += multiples;
+            }
+            if (concatenated > max && is1To9Pandigital(concatenated)) {
+                max = concatenated;
+            }
+        }
+        return max;
     }
 }
