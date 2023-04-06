a69d693aaeeddd811bd17bb5e3148a2763f38a73
romain albisson <albisson.romain@gmail.com>
2023-04-06 13:25:16 +0200

    Simplification

--- project-euler/src/main/java/solutions/Problem008.java
+++ project-euler/src/main/java/solutions/Problem008.java
@@ -1,13 +1,13 @@
 package solutions;
 
 import static utils.FileUtils.P008;
-import static utils.FileUtils.getAllLines;
+import static utils.FileUtils.getAllLinesConcatenated;
 
 public class Problem008 {
 
     public long solve() {
         long max = 0;
-        var series = getAllLines(P008).stream().reduce("", (s1, s2) -> s1 + s2);
+        var series = getAllLinesConcatenated(P008);
         for (int i = 0; i <= series.length() - 13; i++) {
             long product = getProduct(series.substring(i, i + 13));
             if (product > max) {
@@ -18,9 +18,6 @@ public class Problem008 {
     }
 
     private long getProduct(String substring) {
-        if (substring.indexOf('0') > 0) {
-            return 0;
-        }
         long product = 1;
         for (char c : substring.toCharArray()) {
             product *= Character.getNumericValue(c);
