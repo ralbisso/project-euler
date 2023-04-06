8a5a019ce169f844f4093ab858f5972bd0c00463
romain albisson <albisson.romain@gmail.com>
2023-04-06 13:40:00 +0200

    Minor change

--- project-euler/src/main/java/solutions/Problem028.java
+++ project-euler/src/main/java/solutions/Problem028.java
@@ -3,7 +3,7 @@ package solutions;
 public class Problem028 {
 
     public int solve() {
-        int count = 1, sum = 1, increment = 2, spiral = 1001;
+        int sum = 1, count = 1, increment = 2, spiral = 1001;
         for (int number = 3; number <= spiral * spiral; number += increment) {
             if (count++ % 4 == 0) {
                 increment += 2;
