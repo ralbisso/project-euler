007bfae2308253a9b7aa49cf138219fd65231174
romain albisson <albisson.romain@gmail.com>
2023-04-06 13:30:14 +0200

    Minor change

--- project-euler/src/main/java/solutions/Problem009.java
+++ project-euler/src/main/java/solutions/Problem009.java
@@ -1,21 +1,21 @@
 package solutions;
 
-import objects.Triplet;
-
 public class Problem009 {
 
     public int solve() {
-        // We will implement Euclid's formula in the future
         int perimeter = 1000;
         for (int a = 3; a < perimeter; a++) {
             for (int b = a + 1; a + b < perimeter; b++) {
                 int c = perimeter - (a + b);
-                var triplet = new Triplet(a, b, c);
-                if (triplet.isPythagorean()) {
-                    return triplet.product();
+                if (isPythagorean(a, b, c)) {
+                    return a * b * c;
                 }
             }
         }
         return 0;
+    }
+
+    private boolean isPythagorean(int a, int b, int c) {
+        return a * a + b * b == c * c;
     }
 }
