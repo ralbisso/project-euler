7265f05e490c4c19181873c8eae725c3d7a5c96b
romain albisson <albisson.romain@gmail.com>
2023-04-06 15:21:59 +0200

    Problem 36: Double-base palindromes

--- project-euler/src/main/java/solutions/Problem036.java
+++ project-euler/src/main/java/solutions/Problem036.java
@@ -1,8 +1,29 @@
 package solutions;
 
+import static utils.NumberUtils.isPalindromic;
+
 public class Problem036 {
 
     public int solve() {
-        return 0;
+        int sum = 0;
+        for (int i = 1; i < 1000000; i++) {
+            if (isPalindromic(i) && isPalindromicBase2(i)) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    private boolean isPalindromicBase2(int number) {
+        String binary = Integer.toBinaryString(number);
+        int left = 0, right = binary.length() - 1;
+        while (left < right) {
+            if (binary.charAt(left) != binary.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 }
