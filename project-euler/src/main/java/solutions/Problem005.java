738236e838542485721d0e0f7c89a961e56d2d28
romain albisson <albisson.romain@gmail.com>
2023-04-06 13:22:31 +0200

    Missing parenthesis

--- project-euler/src/main/java/solutions/Problem005.java
+++ project-euler/src/main/java/solutions/Problem005.java
@@ -7,7 +7,7 @@ public class Problem005 {
     public int solve() {
         // A number divisible by 1, ... , n is the least common multiple of (1, ... , n)
         // We use the fact that: LCM(a, b) = (a x b) / GCD(a, b)
-        // And: LCM(a0, a1, ... , an) = LCM(a0, LCM(a1, ... , an)
+        // And: LCM(a0, a1, ... , an) = LCM(a0, LCM(a1, ... , an))
         int number = 1;
         for (int divisor = 2; divisor <= 20; divisor++) {
             number = (int) getLCM(divisor, number);
