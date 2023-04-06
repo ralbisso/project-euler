b75214cff737107b5741d2371bde654d7f5771cb
romain albisson <albisson.romain@gmail.com>
2023-04-06 13:32:42 +0200

    No need to over optimize

--- project-euler/src/main/java/solutions/Problem003.java
+++ project-euler/src/main/java/solutions/Problem003.java
@@ -3,11 +3,10 @@ package solutions;
 public class Problem003 {
 
     public int solve() {
-        // The number is odd, no need to check if 2 is a factor
         long number = 600851475143L;
-        int factor = 1;
+        int factor = 2;
         while (number > 1) {
-            factor += 2;
+            factor++;
             while (number % factor == 0) {
                 number /= factor;
             }
