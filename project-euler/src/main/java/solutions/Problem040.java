54ecb1ebdfc094057aa176c7fcfdf8eb4c7f1319
romain albisson <albisson.romain@gmail.com>
2023-04-06 16:15:46 +0200

    Problem 40: Champernowne's constant

--- project-euler/src/main/java/solutions/Problem040.java
+++ project-euler/src/main/java/solutions/Problem040.java
@@ -1,8 +1,31 @@
 package solutions;
 
+import java.util.LinkedList;
+
 public class Problem040 {
 
     public int solve() {
-        return 0;
+        int product = 1, power = 1, digit = 1, number = 1;
+        LinkedList<Integer> constant = new LinkedList<>();
+        for (int i = 1; i < 1000000; i++) {
+            if (i % power == 0) {
+                product *= digit;
+                power *= 10;
+            }
+            if (constant.isEmpty()) {
+                number = feedConstant(constant, number);
+            }
+            digit = constant.removeFirst();
+        }
+        return product;
+    }
+
+    private int feedConstant(LinkedList<Integer> constant, int number) {
+        int addToConstant = ++number;
+        while (addToConstant > 0) {
+            constant.addFirst(addToConstant % 10);
+            addToConstant /= 10;
+        }
+        return number;
     }
 }
