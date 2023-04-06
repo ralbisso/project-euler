005fbe749d4be19b5c0978ab852f019f53e16eb0
romain albisson <albisson.romain@gmail.com>
2023-04-06 13:26:51 +0200

    Minor change

--- project-euler/src/main/java/solutions/Problem007.java
+++ project-euler/src/main/java/solutions/Problem007.java
@@ -5,7 +5,7 @@ import static utils.PrimeUtils.isPrime;
 public class Problem007 {
 
     public int solve() {
-        int count = 2, number = 3;
+        int number = 3, count = 2;
         while (count < 10001) {
             number += 2;
             if (isPrime(number)) {
