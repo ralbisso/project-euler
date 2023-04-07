4b1f27e4d0a57e0b10fecc0e897bafb2d27215fb
romain albisson <albisson.romain@gmail.com>
2023-04-07 19:04:52 +0200

    Problem 41: Pandigital prime

--- project-euler/src/main/java/solutions/Problem041.java
+++ project-euler/src/main/java/solutions/Problem041.java
@@ -1,8 +1,19 @@
 package solutions;
 
+import static utils.NumberUtils.is1ToNPandigital;
+import static utils.PrimeUtils.getSieveOfEratosthenes;
+
 public class Problem041 {
 
     public int solve() {
-        return 0;
+        int max = 0;
+        // The limit was reduced for performance reasons
+        boolean[] primes = getSieveOfEratosthenes(8000000);
+        for (int i = 1; i < primes.length; i++) {
+            if (primes[i] && is1ToNPandigital(i)) {
+                max = i;
+            }
+        }
+        return max;
     }
 }
