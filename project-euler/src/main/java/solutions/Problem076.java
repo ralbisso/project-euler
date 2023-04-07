dec83dfcd020ba867b16ef94aefb1d8974a6313e
romain albisson <albisson.romain@gmail.com>
2023-04-07 22:08:11 +0200

    Problem 76: Counting summations

--- project-euler/src/main/java/solutions/Problem076.java
+++ project-euler/src/main/java/solutions/Problem076.java
@@ -3,6 +3,14 @@ package solutions;
 public class Problem076 {
 
     public int solve() {
-        return 0;
+        int number = 100;
+        int[] ways = new int[number + 1];
+        ways[0] = 1;
+        for (int i = 1; i < number; i++) {
+            for (int j = i; j <= number; j++) {
+                ways[j] += ways[j - i];
+            }
+        }
+        return ways[number];
     }
 }
