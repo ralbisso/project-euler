5355aa315097e775f9d9ee10e87ab29205fc6c2d
romain albisson <albisson.romain@gmail.com>
2023-04-10 15:40:48 +0200

    Refactor

--- project-euler/src/main/java/solutions/Problem012.java
+++ project-euler/src/main/java/solutions/Problem012.java
@@ -5,10 +5,9 @@ import static utils.FactorizationUtils.getNumberOfDivisors;
 public class Problem012 {
 
     public int solve() {
-        int triangle = 1, increment = 1, numberOfDivisors = 1;
-        while (numberOfDivisors < 500) {
+        int triangle = 1, increment = 1;
+        while (getNumberOfDivisors(triangle) < 500) {
             triangle += ++increment;
-            numberOfDivisors = getNumberOfDivisors(triangle);
         }
         return triangle;
     }
