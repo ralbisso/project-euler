70f61a5312743432a3be7d071fd5b57b73bbfa96
romain albisson <albisson.romain@gmail.com>
2023-04-10 13:13:21 +0200

    Minor change

--- project-euler/src/main/java/solutions/Problem040.java
+++ project-euler/src/main/java/solutions/Problem040.java
@@ -1,12 +1,12 @@
 package solutions;
 
-import java.util.LinkedList;
+import java.util.Stack;
 
 public class Problem040 {
 
     public int solve() {
         int product = 1, power = 1, digit = 1, number = 1, limit = 1000000;
-        LinkedList<Integer> constant = new LinkedList<>();
+        Stack<Integer> constant = new Stack<>();
         for (int i = 1; i < limit; i++) {
             if (i % power == 0) {
                 product *= digit;
@@ -15,15 +15,15 @@ public class Problem040 {
             if (constant.isEmpty()) {
                 feedConstant(constant, ++number);
             }
-            digit = constant.removeFirst();
+            digit = constant.pop();
         }
         return product;
     }
 
-    private void feedConstant(LinkedList<Integer> constant, int number) {
+    private void feedConstant(Stack<Integer> constant, int number) {
         int addToConstant = number;
         while (addToConstant > 0) {
-            constant.addFirst(addToConstant % 10);
+            constant.push(addToConstant % 10);
             addToConstant /= 10;
         }
     }
