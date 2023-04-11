055829526c948d306cbacbf9033dcac1dc4932b8
romain albisson <albisson.romain@gmail.com>
2023-04-11 10:05:36 +0200

    Minor change

--- project-euler/src/main/java/solutions/Problem039.java
+++ project-euler/src/main/java/solutions/Problem039.java
@@ -8,7 +8,7 @@ public class Problem039 {
         int perimeter = 0, max = 0, limit = 1000;
         int[] perimeters = new int[limit + 1];
         for (int a = 1; a < limit; a++) {
-            for (int b = a; a + b < limit; b++) {
+            for (int b = a + 1; a + b < limit; b++) {
                 for (int c = b + 1; a + b + c <= limit; c++) {
                     var triangle = new Triangle(a, b, c);
                     int trianglePerimeter = triangle.getPerimeter();
