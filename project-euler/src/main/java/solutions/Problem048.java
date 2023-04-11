5dc4b732ca745ccfb981e2826d1e7784567515e2
romain albisson <albisson.romain@gmail.com>
2023-04-11 13:03:20 +0200

    Problem 48: Self powers

--- project-euler/src/main/java/solutions/Problem048.java
+++ project-euler/src/main/java/solutions/Problem048.java
@@ -2,7 +2,18 @@ package solutions;
 
 public class Problem048 {
 
-    public int solve() {
-        return 0;
+    public long solve() {
+        long sum = 0, mod = (long) Math.pow(10, 10);
+        long[] selfPowers = new long[1000];
+        for (int i = 0; i < selfPowers.length; i++) {
+            int number = i + 1;
+            selfPowers[i] = number;
+            for (int j = 1; j <= i; j++) {
+                selfPowers[i] *= number;
+                selfPowers[i] %= mod;
+            }
+            sum += selfPowers[i];
+        }
+        return sum % mod;
     }
 }
